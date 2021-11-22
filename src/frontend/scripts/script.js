cbdbd420fe52979bf0d0db000088af14b8459546
zhenxiao-yu <markyu0615@gmail.com>
2021-11-22 15:00:40 -0500

    basic version working

--- src/frontend/scripts/script.js
+++ src/frontend/scripts/script.js
@@ -9,6 +9,7 @@ function loadQuestions() {
   xReq.send();
 }
 
+//load quiz answers from backend
 function loadAnswers() {
     let submitBtn = document.getElementById("submit");
     submitBtn.remove();
@@ -22,30 +23,35 @@ function loadAnswers() {
 //populate quiz questions
 function displayQuestions() {
   if (this.readyState == 4 && this.status == 200) {
-    let questionDiv = document.getElementById("questionDiv");
-    let questionList = JSON.parse(this.responseText);
+    let question_block = document.getElementById("question_block");
+    //parse question.JSON as question banks
+    let questions = JSON.parse(this.responseText);
+    //define empty page content
     let content = "";
-
+    content += "<div id='grade'></div>";
+    //define a form to contain question
     content += "<form action=javascript:loadAnswers()>";
-
-    for (q of questionList) {
-      content += "<p>" + q.stem + "</p>";
-
-      for (n of q.options) {
-        content +=
-          '<input type="radio" id="answer ' + n + '" name="' + q.stem + '">';
-        content += '<label for="answer ' + n + '">' + n + "</label></br>";
+    //iterate through each question in question bank
+    for (question of questions) {
+        //fill in each label with question text
+      content += "<label class='question_text'>" + question.stem + "</label>";
+        //fill in each option 
+      for (n of question.options) { //create correct number of radio buttons based on the size of options
+        //option button
+        content += '<ul><input type="radio" id="answer ' + n + '" name="' + question.stem + '">';
+        //option label
+        content += '<label for="answer ' + n + '">' + n + "</label></ul>";
       }
-
-      content += "</br><div id='answerDiv " + q.stem + "'></div>";
+      //question response
+      content += "<div id='answerDiv " + question.stem + "'></div>";
+      //division line to format questions
+      content += "<hr>";
     }
-
+    
     content += '</br><input type="submit" value="Submit" id="submit">';
     content += "</form></br>";
 
-    content += "<div id='gradeDiv'></div></br>";
-
-    questionDiv.innerHTML = content;
+    question_block.innerHTML = content;
   }
 }
 
@@ -53,24 +59,24 @@ function displayQuestions() {
 //populate quiz options
 function displayAnswers() {
   if (this.readyState == 4 && this.status == 200) {
-    let questionList = JSON.parse(this.responseText);
-    let gradeDiv = document.getElementById("gradeDiv");
+    let questions = JSON.parse(this.responseText);
+    let gradeDiv = document.getElementById("grade");
     let correctAnswers = 0;
     let totalQuestions = 0;
 
-    for (q of questionList) {
+    for (question of questions) {
       let feedback = "";
-      let answerDiv = document.getElementById("answerDiv " + q.stem);
+      let answerDiv = document.getElementById("answerDiv " + question.stem);
 
-      let radioButtons = document.getElementsByName("" + q.stem);
+      let radioButtons = document.getElementsByName("" + question.stem);
 
       for (let i = 0; i < radioButtons.length; i++) {
         if (radioButtons[i].checked == true) {
-          if (i == q.answerIndex) {
-            feedback = "Correct!";
+          if (i == question.answerIndex) {
+            feedback = "Correct";
             correctAnswers++;
           } else {
-            feedback = "Incorrect! ";
+            feedback = "Incorrect";
           }
         }
       }
@@ -80,6 +86,6 @@ function displayAnswers() {
     }
 
     gradeDiv.innerHTML =
-      "Your grade is: " + (correctAnswers / totalQuestions) * 100 + "%!";
+      "You answered " + correctAnswers + "/" + totalQuestions + " questions correctly. (" + 100*(correctAnswers / totalQuestions) + "%)";
   }
 }
